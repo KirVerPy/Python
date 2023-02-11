dc0598b2caa543916faa4d2120b2e294c5d1ab31
KirVerPy <kirill.vereshchagin@mail.ru>
2023-02-11 10:59:55 +0300

    first commit
    1 com

---
+++ 1 lesson/first.py
@@ -0,0 +1,11 @@
+a = 5
+b = 10
+print(a > b)
+
+c = 4
+v = 5
+print(c + v)
+
+g = 5
+h = int(input())
+print(g +h)
---
+++ 2 lesson/first2.py
@@ -0,0 +1,10 @@
+print(9//3)
+
+print(11%3)
+
+print(5**2)
+
+print( 1 + 1 == 2 or 1 + 1 == 3)
+print( 1 + 2 == 3 and 8 + 3 == 3)
+print(1 + 2== 3 and not 8 + 3 == 3)
+print (5 + 5 == 10 not 4 + 6 == 11)
