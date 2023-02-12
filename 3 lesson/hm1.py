65d4d013961d4d71633fb345450bfd20d49ff1c4
KirVerPy <kirill.vereshchagin@mail.ru>
2023-02-12 16:26:33 +0300

    3 lesson2.0
    3 com

--- 3 lesson/hm1.py
+++ 3 lesson/hm1.py
@@ -1,8 +1,7 @@
 a = int(input())
 b = 100
-a != b
-if a > b:
-    print("Число больше 100")
+if a >= b:
+    print("Число больше или ровно 100")
 elif 50 < a and a < b:
         print("Число меньше 100, но больше 50")
 else:
