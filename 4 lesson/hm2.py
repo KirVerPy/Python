b4246bdae7b1d9c8054a7868a53f41dec436e196
KirVerPy <kirill.vereshchagin@mail.ru>
2023-02-17 11:29:28 +0300

    4 lesson
    4 com

---
+++ 4 lesson/hm2.py
@@ -0,0 +1,6 @@
+a = 101
+while a:
+    a -= 1
+    if a % 2 != 0:
+        continue 
+    print (a)
---
+++ 4 lesson/hm3.py
@@ -0,0 +1,6 @@
+a = 300
+while a:
+    a -= 1
+    if a % 2 != 1:
+        continue 
+    print (a)
